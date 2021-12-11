15a02ce0aa0639beaeb15d196b00a09145876893
cristianadondero <91587462+cristianadondero@users.noreply.github.com>
2021-12-11 11:15:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int dimensione;
+    cin>>dimensione;
+    float array[dimensione];
+    for (int i=0;i<dimensione;i++){
+        cin>>array[i];
+    }
+    float ris= max (dimensione,array);
+    cout<<ris<<endl;
+    return 0;
 }
