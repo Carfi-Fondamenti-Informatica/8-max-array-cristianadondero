9d2c9e9734b774eca36b4b9217147177996b9ae6
cristianadondero <91587462+cristianadondero@users.noreply.github.com>
2021-12-11 11:11:48 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+#include <cmath>
+#include <limits>
+float max(int a,float numeri[]){
+    float m=-INFINITY;
+    for (int i=0;i<a;i++){
+        if (numeri[i]>m){
+            m=numeri [i];
+        }
+    }
+    return m;
+}
